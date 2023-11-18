7820b455a5e3ffa2867a5250438d880093167640
anm11511522ubca045 <151377087+anm11511522ubca045@users.noreply.github.com>
2023-11-18 05:16:23 -0800

    Add files via upload

---
+++ Challenge-3.1/main.py
@@ -0,0 +1,22 @@
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input.
+"""
+
+
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
